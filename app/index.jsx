37adb38620400aa9aca39af337b024dc2fedadbb
ahmedzacki <ahmedxandulle01@gmail.com>
2025-01-09 18:10:39 -0500

    Created the data

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// I am going to create a simple CRUD app using React Native and Expo.
+// I will use the following technologies:
+// - React Native
+// - Expo
+// - React Navigation
+// - React Native Elements
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        
+      </View>
+      
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
